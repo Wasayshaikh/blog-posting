f11fda94f178194225ef3ef3f033391f667e934d
Wasayshaikh <abdulwasayshaikh1999@gmail.com>
2025-06-22 02:51:34 +0800

    separate file for auth thunks

---
+++ src/redux/thunkFunctions/Auth.ts
@@ -0,0 +1,82 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export type user = {
+    email: string,
+    name: string,
+    username: string
+}
+
+export type AuthError = {
+    message: string;
+    status?: string;
+    error?: string;
+};
+
+
+export type AuthPayload = {
+    user: user;
+    token: string | null;
+};
+
+type LogoutPayload = {status:string,message:string}
+
+// Credentials Types
+type LoginCredentials= { email: string; password: string;}
+type RegisterCredentials ={ email: string; password: string; username: string; name:string }
+type LogoutCredentials={token:string}
+
+export const login = createAsyncThunk<AuthPayload, LoginCredentials,{ rejectValue: AuthError }>("auth/login", async (credentials: { email: string, password: string }, thunkAPI) => {
+    try {
+        const response = await fetch("http://127.0.0.1:8000/api/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(credentials)
+        });
+        if (!response.ok) {
+            const errorData = await response.json();
+            return thunkAPI.rejectWithValue(errorData);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error: any) {
+        return thunkAPI.rejectWithValue({ message: error.message });
+    }
+
+})
+
+export const register = createAsyncThunk<AuthPayload, RegisterCredentials,{ rejectValue: AuthError }>("auth/register", async (credentials: { email: string, password: string, username: string, name:string }, thunkAPI) => {
+        try {
+            const response = await fetch("http://127.0.0.1:8000/api/register", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(credentials)
+            });
+            if (!response.ok) {
+                const errorData = await response.json();
+                return thunkAPI.rejectWithValue(errorData);
+            }
+            const data = await response.json();
+            return data;
+        } catch (error: any) {
+            return thunkAPI.rejectWithValue({ message: error.message });
+        }
+
+    })
+export const logout = createAsyncThunk<LogoutPayload, LogoutCredentials,{ rejectValue: AuthError }>("auth/logout", async (credentials: { token: string }, thunkAPI) => {
+    try {
+        const response = await fetch("link", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(credentials)
+        });
+        if (!response.ok) {
+            const errorData = await response.json();
+            return thunkAPI.rejectWithValue(errorData);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error: any) {
+        return thunkAPI.rejectWithValue({ message: error.message });
+    }
+
+})
