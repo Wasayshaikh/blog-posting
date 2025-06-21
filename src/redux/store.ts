eb3ba464d89e2b972fe82797bb5ff8a33e11b182
Wasayshaikh <abdulwasayshaikh1999@gmail.com>
2025-06-22 02:52:37 +0800

    setup and config the persister

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authSlice from './slices/AuthSlice'
+import { persistReducer } from "redux-persist";
+import localStorage from "redux-persist/es/storage";
+import persistStore from "redux-persist/es/persistStore";
 
 
-const store = configureStore({
-  reducer: {
-    auth:authSlice
-  },
+
+const persistConfig =  {
+  key:"root",
+  storage:localStorage,
+  whitelist:['auth']
+}
+const rootReducer = combineReducers({
+  auth:authSlice,
 })
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+export const store = configureStore({
+  reducer: persistedReducer,
+   middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: false,
+      }),
+})
+
+const persistor = persistStore(store);
+
+export default persistor;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
