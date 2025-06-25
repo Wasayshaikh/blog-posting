d763b85f698d92d5d50becda39a186ad209d5c01
Wasayshaikh <abdulwasayshaikh1999@gmail.com>
2025-06-26 00:21:50 +0800

    add form submission

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,31 +1,54 @@
 import { useDispatch, useSelector } from "react-redux"
 import { login } from "../redux/thunkFunctions/Auth";
 import type { RootState } from "../redux/store";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Login() {
-   const dispatch = useDispatch<any >();
+    const navigate = useNavigate();
+    const dispatch = useDispatch<any>();
     // const auth_check = useSelector(auth)
     // console.log(auth_check)
-     const auth = useSelector((state: RootState) => state.auth);
-     console.log(auth)
+    const auth = useSelector((state: RootState) => state.auth);
+    console.log(auth)
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(formData)
+        // const result = await dispatch(login(formData));
+        // email: "abc@abc.abc", password: "Admin!23456" }
+            const result = dispatch(login(formData))
+        if (login.fulfilled.match(result)) {
+            navigate('/');}
+        
+    };
+
     return (
 
         <div className="min-h-screen flex items-center justify-center">
-            <button onClick={()=>{dispatch(login({email:"abc@abc.abc",password:"Admin!23456"}))}}>Click</button>
+           
             <div className="bg-white/90 p-8 rounded-lg shadow-lg w-full max-w-md">
                 <h2 className="text-2xl font-semibold text-center text-gray-800 mb-6">Login</h2>
 
-                <form action="#" method="POST" className="space-y-4">
+                <form onSubmit={handleSubmit} action="#" method="POST" className="space-y-4">
                     <div>
                         <label className="block text-sm text-gray-700 mb-1">Email</label>
-                        <input type="email" id="email" name="email" required
+                        <input type="email" name="email" onChange={handleChange} required
                             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600" />
                     </div>
 
                     <div>
                         <label className="block text-sm text-gray-700 mb-1">Password</label>
-                        <input type="password" id="password" name="password" required
+                        <input type="password" name="password" onChange={handleChange} required
                             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600" />
                     </div>
 
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,38 @@
-import { Link } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom"
+import type { RootState } from "../redux/store";
+import { useState } from "react";
+import { register } from "../redux/thunkFunctions/Auth";
 
 function Register() {
+     const navigate = useNavigate();
+        const dispatch = useDispatch<any>();
+        // const auth_check = useSelector(auth)
+        // console.log(auth_check)
+        const auth = useSelector((state: RootState) => state.auth);
+        console.log(auth)
+        const [formData, setFormData] = useState({
+            email: '',
+            password: '',
+            username:'',
+            name:''
+        });
+        const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+            setFormData({
+                ...formData,
+                [e.target.name]: e.target.value
+            });
+        };
+        const handleSubmit = async (e: React.FormEvent) => {
+            e.preventDefault();
+            console.log(formData)
+            // const result = await dispatch(login(formData));
+            // email: "abc@abc.abc", password: "Admin!23456" }
+                const result = dispatch(register(formData))
+            if (register.fulfilled.match(result)) {
+                navigate('/');}
+            
+        };
   return (
     <div className="min-h-screen flex items-center justify-center">
             <div className="bg-white/90 p-8 rounded-lg shadow-lg w-full max-w-md">
@@ -9,13 +41,23 @@ function Register() {
                 <form action="#" method="POST" className="space-y-4">
                     <div>
                         <label className="block text-sm text-gray-700 mb-1">Email</label>
-                        <input type="email" id="email" name="email" required
+                        <input type="email"  name="email"  onChange={handleChange} required
                             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600" />
                     </div>
 
                     <div>
                         <label className="block text-sm text-gray-700 mb-1">Password</label>
-                        <input type="password" id="password" name="password" required
+                        <input type="text"  name="username" onChange={handleChange} required
+                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600" />
+                    </div>
+                     <div>
+                        <label className="block text-sm text-gray-700 mb-1">Password</label>
+                        <input type="text"  name="name" onChange={handleChange} required
+                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600" />
+                    </div>
+                     <div>
+                        <label className="block text-sm text-gray-700 mb-1">Password</label>
+                        <input type="password" name="password" onChange={handleChange} required
                             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600" />
                     </div>
 
