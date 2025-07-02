911e59b9e38b932b7870b9f83efd9469972fa061
Wasayshaikh <abdulwasayshaikh1999@gmail.com>
2025-07-03 01:21:14 +0800

    add route and slug

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Blog, Home, Login, Register } from './pages'
+import Header from './components/Header'
 
 function App() {
 
@@ -7,9 +8,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="register" element={<Register />} />
+        <Route path="/blog/:slug" element={<Blog />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="*" element={<><Header /><p>Error: 404 Not Found</p></>} />
       </Routes>
     </BrowserRouter>
   )
