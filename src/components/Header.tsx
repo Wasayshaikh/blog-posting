fa41536aa98d69ede5629cae5e3fb9608e792f6f
Wasayshaikh <abdulwasayshaikh1999@gmail.com>
2025-07-03 01:20:17 +0800

    add logout feature

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import type { RootState } from '../redux/store';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faUser,faAr } from '@fortawesome/free-regular-svg-icons'
 import { faRightFromBracket,faUser } from '@fortawesome/free-solid-svg-icons';
+import { logout } from '../redux/thunkFunctions/Auth';
 export default function Header() {
+     const dispatch = useDispatch<any>();
     const isLogged = useSelector((state:RootState)=> state.auth.isLogged);
+     const token = useSelector((state:RootState)=> state.auth.token);
     return (
         <header className="bg-gray-900 text-white shadow-md">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -29,12 +32,12 @@ export default function Header() {
 
                     {/* <!-- Right: Login Icon --> */}
                     <div className="flex-shrink-0">
-                        <Link to={"/login"} title='Home'>
-                      { isLogged?(<FontAwesomeIcon icon={faRightFromBracket} />):(<FontAwesomeIcon icon={faUser} />)
+                        
+                      { isLogged?(<FontAwesomeIcon icon={faRightFromBracket} onClick={()=>{dispatch(logout({token:token}))}}/>):(<Link to={"/login"} title='Home'><FontAwesomeIcon icon={faUser} /></Link>)
 
                       }
                            
-                        </Link>
+                        
                     </div>
 
                 </div>
